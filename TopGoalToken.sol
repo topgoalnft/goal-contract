06da876155a2261d7926ccb16976fc4e1c253936
Yang Ye <yeyangever@gmail.com>
2022-12-16 06:07:15 +0800

    update goal token

--- TopGoalToken.sol
+++ TopGoalToken.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.6.12;
+pragma solidity =0.8.0;
 
-import "./lib/token/BEP20/BEP20.sol";
-import "./lib/access/Pausable.sol";
+import "./BEP20.sol";
 
 contract TopGoalToken is BEP20, Pausable {
 
@@ -161,7 +160,7 @@ contract TopGoalToken is BEP20, Pausable {
         address signatory = ecrecover(digest, v, r, s);
         require(signatory != address(0), "Goal::delegateBySig: invalid signature");
         require(nonce == nonces[signatory]++, "Goal::delegateBySig: invalid nonce");
-        require(now <= expiry, "Goal::delegateBySig: signature expired");
+        require(block.timestamp <= expiry, "Goal::delegateBySig: signature expired");
         return _delegate(signatory, delegatee);
     }
 
@@ -242,7 +241,7 @@ contract TopGoalToken is BEP20, Pausable {
                 // decrease old representative
                 uint32 srcRepNum = numCheckpoints[srcRep];
                 uint256 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
-                uint256 srcRepNew = srcRepOld.sub(amount);
+                uint256 srcRepNew = SafeMath.sub(srcRepOld, amount);
                 _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
             }
 
@@ -250,7 +249,7 @@ contract TopGoalToken is BEP20, Pausable {
                 // increase new representative
                 uint32 dstRepNum = numCheckpoints[dstRep];
                 uint256 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
-                uint256 dstRepNew = dstRepOld.add(amount);
+                uint256 dstRepNew = SafeMath.add(dstRepOld, amount);
                 _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
             }
         }
@@ -281,7 +280,7 @@ contract TopGoalToken is BEP20, Pausable {
         return uint32(n);
     }
 
-    function getChainId() internal pure returns (uint) {
+    function getChainId() internal view returns (uint) {
         uint256 chainId;
         assembly { chainId := chainid() }
         return chainId;
